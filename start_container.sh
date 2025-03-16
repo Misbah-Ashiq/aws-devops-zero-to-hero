89fc69b606a24deec7f88ba7b40204a1917d18e9
misbah-ashiq <misbahashiq46@gmail.com>
2025-03-16 15:50:16 +0500

    Update start_container.sh

--- start_container.sh
+++ start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-echo
+docker pull docker push ahmadali616/sample-python
 
 # Run the Docker image as a container
-echo
+docker run -d -p 5000:5000 ahmadali616/sample-python
