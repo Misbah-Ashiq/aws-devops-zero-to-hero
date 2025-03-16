0a0b957f2801e87c69525339d6961882c141a3f6
misbah-ashiq <misbahashiq46@gmail.com>
2025-03-16 16:40:45 +0500

    Update start_container.sh

--- start_container.sh
+++ start_container.sh
@@ -5,4 +5,4 @@ set -e
 docker pull docker push ahmadali616/sample-python
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 ahmadali616/sample-python
+docker run -d -p 5000:5000 ahmadali616/sample-python 
